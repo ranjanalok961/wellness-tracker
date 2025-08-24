7539e7e469c3c8dc8db6fded04f6238b08e64dda
Ranjan Alok <103888844+ranjanalok961@users.noreply.github.com>
2025-08-25 00:33:44 +0530

    Fix: useEffect fetchMetrics dependency issue with useCallback

--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , useCallback} from "react";
 import { auth, db } from "../Firebase/firebase";
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import {
